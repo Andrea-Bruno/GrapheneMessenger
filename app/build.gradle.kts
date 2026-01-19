467af890b4aa6c85344197d0dfb9107fe6ca8cc5
Andrea Bruno <andrea_bruno@hotmail.com>
2026-01-19 18:01:49 +0400

    Regular Update 19/01/2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
   id("graphene")
 }
 
-val canonicalVersionCode = 251231 // alias: 1633
-val canonicalVersionName = "25.12.31" // alias: "7.68.5"
+val canonicalVersionCode = 260119 // alias: 1633
+val canonicalVersionName = "26.01.19" // alias: "7.68.5"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 val grapheneRevision = 1
